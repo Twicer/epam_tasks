f06e92dae41824111765637b5a2887eb34c4b400
Zviger <mbs131119999@gmail.com>
2019-10-22 23:10:29 +0300

    Task_4.5 is reworked

--- python_tasks/task_4.5.py
+++ python_tasks/task_4.5.py
@@ -1,3 +1,6 @@
+import functools
+
+
 def remember_result(func):
     last_result = None
 
@@ -11,9 +14,7 @@ def remember_result(func):
 
 @remember_result
 def sum_list(*args):
-    result = args[0]
-    for item in args[1:]:
-        result += item
+    result = functools.reduce(lambda a, b: a + b, args)
     print(f"Current result = {result}")
     return result
 
@@ -22,3 +23,4 @@ if __name__ == "__main__":
     sum_list(5, 6)
     sum_list(7, 5)
     sum_list("asd", "dasd")
+    sum_list(1, 5)
